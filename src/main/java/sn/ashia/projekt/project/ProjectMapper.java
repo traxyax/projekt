e25ce98552c444b870d66784f3492965428481d0
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-26 12:43:05 +0000

    adding project mapper

---
+++ src/main/java/sn/ashia/projekt/project/ProjectMapper.java
@@ -0,0 +1,23 @@
+package sn.ashia.projekt.project;
+
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.Set;
+
+@Mapper(
+        componentModel = "spring",
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+        uses = {ProjectMapper.class}
+)
+public interface ProjectMapper {
+    ProjectDTO toDTO(Project entity);
+    Project toEntity(ProjectDTO dto);
+
+    Set<ProjectDTO> toDTO(List<Project> entities);
+    Set<Project> toEntity(List<ProjectDTO> dtos);
+
+    Set<ProjectDTO> toDTO(Set<Project> entities);
+    Set<Project> toEntity(Set<ProjectDTO> dtos);
+}
