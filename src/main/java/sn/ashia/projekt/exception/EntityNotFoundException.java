1306962244af14d48cdb73e988007ace667df5aa
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-26 11:08:17 +0000

    adding entity not found exception

---
+++ src/main/java/sn/ashia/projekt/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package sn.ashia.projekt.exception;
+
+public class EntityNotFoundException extends ApiException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
