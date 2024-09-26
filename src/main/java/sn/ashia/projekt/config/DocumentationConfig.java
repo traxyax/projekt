39e3d31c633f385d7df97e5e32df282c1069946c
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-26 22:41:06 +0000

    adding documentation config

---
+++ src/main/java/sn/ashia/projekt/config/DocumentationConfig.java
@@ -0,0 +1,44 @@
+package sn.ashia.projekt.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"dev", "pre-prod"})
+@Configuration
+public class DocumentationConfig {
+    @Bean
+    public GroupedOpenApi apiOpenApi() {
+        String[] paths = {"/api/**"};
+        return GroupedOpenApi.builder().group("api").pathsToMatch(paths).build();
+    }
+
+    @Bean
+    public GroupedOpenApi userOpenApi() {
+        return generateGroupedOpenApi("user", "users");
+    }
+
+    @Bean
+    public GroupedOpenApi projectOpenApi() {
+        return generateGroupedOpenApi("project", "projects");
+    }
+
+    @Bean
+    public OpenAPI projektOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Projekt API")
+                        .description("Project management")
+                );
+    }
+
+    private GroupedOpenApi generateGroupedOpenApi(String group, String path) {
+        return GroupedOpenApi.builder()
+                .group(group)
+                .pathsToMatch("/api/" + path + "/**")
+                .addOpenApiCustomizer(openApi -> openApi.info(new Info().title(group + " api")))
+                .build();
+    }
+}
