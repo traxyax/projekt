248b2db2886116313f965d77006679faabe025b2
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-26 12:38:34 +0000

    add project dto

---
+++ src/main/java/sn/ashia/projekt/project/ProjectDTO.java
@@ -0,0 +1,25 @@
+package sn.ashia.projekt.project;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import sn.ashia.projekt.user.User;
+
+import java.time.Instant;
+import java.util.Set;
+
+public record ProjectDTO(
+        Long id,
+        @NotBlank
+        String title,
+        @NotNull
+        Integer durationMonths,
+        String genderMarker,
+        String mainPartner,
+        Double amountFef,
+        Double amountCofinancing,
+        String type,
+        ProjectStatus status,
+        Instant createdDate,
+        Instant lastModifiedDate,
+        Set<User> managers
+) { }
