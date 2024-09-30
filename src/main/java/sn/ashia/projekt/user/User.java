55777a7c79718c77204047be2130d76f2bc6c6c9
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-30 10:50:05 +0000

    update getName method if email is null

--- src/main/java/sn/ashia/projekt/user/User.java
+++ src/main/java/sn/ashia/projekt/user/User.java
@@ -42,6 +42,9 @@ public class User extends AbstractEntity {
     }
 
     public String getName() {
+        if (email == null) {
+            return null;
+        }
         String name = email.split("@")[0];
         name = String.join(" ", name.split("\\."));
         name = name.toUpperCase();
