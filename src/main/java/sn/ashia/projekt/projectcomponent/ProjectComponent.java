0a72352498193bbebabde37fadd552bc34a97110
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-30 15:44:40 +0000

    updating entity

--- src/main/java/sn/ashia/projekt/projectcomponent/ProjectComponent.java
+++ src/main/java/sn/ashia/projekt/projectcomponent/ProjectComponent.java
@@ -1,4 +1,49 @@
 package sn.ashia.projekt.projectcomponent;
 
-public class ProjectComponent {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import sn.ashia.projekt.enumeration.GlobalStatus;
+import sn.ashia.projekt.persistance.AbstractEntity;
+import sn.ashia.projekt.project.Project;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public class ProjectComponent extends AbstractEntity {
+    @NotNull
+    private Integer number;
+
+    @NotEmpty
+    @Column(columnDefinition = "TEXT")
+    private String objective;
+
+    private GlobalStatus status;
+
+    @NotNull
+    @ManyToOne
+    private Project project;
+
+    public void setStatus(GlobalStatus status) {
+        this.status = Objects.requireNonNullElse(status, GlobalStatus.KO);
+    }
+
+    public Long getProjectId() {
+        return project.getId();
+    }
 }
