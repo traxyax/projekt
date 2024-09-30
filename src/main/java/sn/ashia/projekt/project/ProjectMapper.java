f3f35c5958e25de9b9b101af83c7832e9e45099a
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-30 10:06:38 +0000

    use UserMapper

--- src/main/java/sn/ashia/projekt/project/ProjectMapper.java
+++ src/main/java/sn/ashia/projekt/project/ProjectMapper.java
@@ -1,13 +1,14 @@
 package sn.ashia.projekt.project;
 
 import org.mapstruct.Mapper;
+import sn.ashia.projekt.user.UserMapper;
 
 import java.util.List;
 import java.util.Set;
 
 @Mapper(
         componentModel = "spring",
-        uses = {ProjectMapper.class}
+        uses = {UserMapper.class}
 )
 public interface ProjectMapper {
     ProjectDTO toDTO(Project entity);
