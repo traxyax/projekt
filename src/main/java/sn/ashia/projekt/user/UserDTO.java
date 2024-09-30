6905e13290ee84cb5af28cf75824eb5d38f03e6e
Aboubacar TRAORE <traxyax@gmail.com>
2024-09-30 10:28:23 +0000

    remove validation to dto

--- src/main/java/sn/ashia/projekt/user/UserDTO.java
+++ src/main/java/sn/ashia/projekt/user/UserDTO.java
@@ -1,13 +1,10 @@
 package sn.ashia.projekt.user;
 
-import jakarta.validation.constraints.Email;
-
 import java.util.Set;
 
 public record UserDTO(
         Long id,
         String name,
-        @Email
         String email,
         Set<UserRole> roles
 ) { }
